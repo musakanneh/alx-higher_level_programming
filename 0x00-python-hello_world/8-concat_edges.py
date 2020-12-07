78bfe3bbd3bf88a2d12c6f45961bc3aba5f79084
musakanneh <mkanneh17@alustudent.com>
2020-12-07 09:37:45 +0200

    First commit

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+         language that combines remarkable power with very clear syntax"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LIN 
+print(str)
