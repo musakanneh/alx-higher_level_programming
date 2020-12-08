229b896641c3a0fd0048458b7dba9a66db09742c
musakanneh <mkanneh17@alustudent.com>
2020-12-08 21:41:37 +0200

    Attempted all tasks

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,9 @@
+#!/usr/bin/python3
+
 def uppercase(str):
-    pass
+    for i in range(len(str)):
+        uni_code = ord(str[i])
+        if uni_code >= 97 and uni_code <= 122:
+            uni_code = uni_code - 32
+        print("{}".format(chr(uni_code)), end='')
+    print()
