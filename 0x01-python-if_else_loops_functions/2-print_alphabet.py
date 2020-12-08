2e68da4809c50abccd49d1a9a80d126dd85bcb1f
musakanneh <mkanneh17@alustudent.com>
2020-12-08 18:56:58 +0200

    Attepmted task-2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == 113 or i == 101:
-        continue
-    print("{}".format(chr(i)), end="")
+    if i is not 113 and  i is not 101:
+        print("{}".format(chr(i)), end="")
