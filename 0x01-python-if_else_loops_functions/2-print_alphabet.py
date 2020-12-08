d9ff1e6260e77d79780084fea4f3fae1b141f1a6
musakanneh <mkanneh17@alustudent.com>
2020-12-08 19:09:04 +0200

    Attepmted task-2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == 113 or i == 101:
+    if chr(i) == 'e' or chr(i) == 'q':
         continue
-    else:
-        print("{}".format(chr(i)), end='')
+    print("{}".format(chr(i)), end='')
