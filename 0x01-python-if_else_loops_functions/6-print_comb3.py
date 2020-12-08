bb117eff9ce5aa94f5e915afbc6586345f6c6c1a
musakanneh <mkanneh17@alustudent.com>
2020-12-08 22:43:48 +0200

    Fixed task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-for digit1 in range(0, 10):
-    for digit2 in range(digit1 + 1, 10):
-        if digit1 == 8 and digit2 == 9:
-            print("{}{}".format(digit1, digit2))
+for x in range(0, 10):
+    for y in range(x + 1, 10):
+        if (not (x == 8 and y == 9)):
+            print("{}{}".format(x, y), end=", ")
         else:
-            print("{}{}".format(digit1, digit2), end=", ")`
+            print("{}{}".format(x, y))
