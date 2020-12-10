88965a28ec30d6fd220a9c3b04cf1da9695db3b8
musakanneh <mkanneh17@alustudent.com>
2020-12-10 22:33:31 +0200

    Python - import & modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-# 3-infinite_add.py
 
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
     import sys
 
     total = 0
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# 5-variable_load.py
 
 
 if __name__ == "__main__":
@@ -7,5 +6,4 @@ if __name__ == "__main__":
     Print the value of variable a from variable_load_5
     """
     from variable_load_5 import a
-
     print(a)
