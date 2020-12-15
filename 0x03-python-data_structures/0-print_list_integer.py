9de3ad909b262b6a3de1e4e56fad89a3caba94d6
musakanneh <mkanneh17@alustudent.com>
2020-12-15 18:11:05 +0200

    Added Lists, Tuples

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -6,4 +6,4 @@ def print_list_integer(my_list=[]):
     A function that prints all integers of a list.
     """
     for i in range(len(my_list)):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
