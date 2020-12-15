adcf2a9b5079bc00b40930011a6c420b4b59c399
musakanneh <mkanneh17@alustudent.com>
2020-12-15 19:13:52 +0200

    Lists, Tuples

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -6,6 +6,6 @@ def replace_in_list(my_list, idx, element):
     A function that replaces an element of 
     a list at a specific position
     """ 
-    if idx >= 0 and idx < len(my_list) - 1:
+    if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
     return (my_list)
