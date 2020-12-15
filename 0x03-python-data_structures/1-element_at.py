4b21a06904f3df7a27762a6982d692ccdca906c0
musakanneh <mkanneh17@alustudent.com>
2020-12-15 18:46:58 +0200

    Added Lists, Tuples

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,8 +5,6 @@ def element_at(my_list, idx):
     """
      A function that retrieves an element from a list like in C.
     """
-    list_idx = my_list[idx]  
-    for i in range(list_idx):
-        if idx > list_idx or idx < list_idx:
-           return None
-        return (my_list[idx])
+    if idx < 0 or idx > len(my_list) - 1:
+        return None
+    return (my_list[idx])
