28015e2e6b82b4b7cbe8a9524563f1cd95a64556
musakanneh <mkanneh17@alustudent.com>
2020-12-15 19:55:58 +0200

    Lists, Tuples

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 4-new_in_list.py
+
+
+def new_in_list(my_list, idx, element):
+    """
+    A function that replaces an element in a list at 
+    a specific position without modifying the original list
+    """
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
+    copy = [x for x in my_list]
+    copy[idx] = element
+    return copy
