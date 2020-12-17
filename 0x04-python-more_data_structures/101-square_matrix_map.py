da2c7a82d83d3fc9590e3c9e025631677c58fc65
musakanneh <mkanneh17@alustudent.com>
2020-12-17 21:56:24 +0200

    Added more advanced tasks

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -6,4 +6,4 @@ def square_matrix_map(matrix=[]):
     a function that computes the square value of
     all integers of a matrix using map
     """
-    return(list(map(lambda i: list(map(lambda x: x * x, i)), matrix)))
+    return list(map(lambda row: list(map(lambda x: (x*x), row)), matrix))
---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+    """
+    A function that computes the square value of
+    all integers of a matrix using map
+    """
+    a_list = list(a_dictionary.keys())
+    for key in a_list:
+        if a_dictionary[key] == value:
+            del a_dictionary[key]
+    return a_dictionary
