74d1f7546834e34ed92bff2e46abcc9f29615ac7
musakanneh <mkanneh17@alustudent.com>
2021-01-04 17:46:43 +0200

    Quick fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,7 +10,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[element]), end="")
             list_count += 1
-        except Exception:
-            break;
+        except IndexError:
+            break
     print("")
     return list_count
