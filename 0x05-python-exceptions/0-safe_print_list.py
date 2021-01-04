824dbac892650849a7529c66e7bacd4ef4e73a7c
musakanneh <mkanneh17@alustudent.com>
2021-01-04 17:36:58 +0200

    Quick fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,7 +10,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[element]), end="")
             count += 1
-        except Exception as e:
+        except Exception:
             break;
     print("")
     return count
