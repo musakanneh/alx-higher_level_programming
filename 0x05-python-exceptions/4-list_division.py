d1e2f4135f8ee6324a0379343d9ee59d360e5cb9
musakanneh <mkanneh17@alustudent.com>
2021-01-04 22:19:02 +0200

    Attempted all tasks

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -18,6 +18,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         except IndexError:
             print("out of range")
+            result = 0
         finally:
             temp_list.append(result)
     return temp_list
