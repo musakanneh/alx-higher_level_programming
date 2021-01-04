378307910779e5f07180bb4cf7e0bb883ab74d51
musakanneh <mkanneh17@alustudent.com>
2021-01-04 17:41:44 +0200

    Quick fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,12 +5,12 @@ def safe_print_list(my_list=[], x=0):
     """
     A function that prints x elements of a list.
     """
-    count = 0
+    list_count = 0
     for element in range(x):
         try:
-            print("{}".format(my_list[element]), end="")
-            count += 1
+            print("{:d}".format(my_list[element]), end="")
+            list_count += 1
         except Exception:
             break;
     print("")
-    return count
+    return list_count
