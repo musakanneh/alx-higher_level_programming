c568f960c602bb72098da71b39e40b3dc35b08ee
musakanneh <mkanneh17@alustudent.com>
2021-01-05 21:56:02 +0200

    Classes and Objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -61,8 +61,7 @@ class Square:
         if self.size == 0:
             print("")
             return
-        for i in range(0, self.__position[1]):
-            [print("")]
+        [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.size)]
