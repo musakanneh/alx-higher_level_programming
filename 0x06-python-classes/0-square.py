ad38bc602aafcdb25f8703410f6ad3129da64f47
musakanneh <mkanneh17@alustudent.com>
2021-01-05 11:37:11 +0200

    Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,2 +1,5 @@
+#!/usr/bin/python3
+
+
 class Square:
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+class Square:
+    """Square class with a private attribute -
+    size.
+
+    """
+    __size = None
+
+    def __init__(self, __size):
+        self.__size = __size
