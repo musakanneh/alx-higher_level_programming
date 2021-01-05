1a670b2cb0876de0592b703b532aab0d53c76bf1
musakanneh <mkanneh17@alustudent.com>
2021-01-05 17:28:02 +0200

    Classes and Objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,5 +3,15 @@
 
 
 class Square:
-    """And empty square class with no artribute."""
-    pass
+    """And empty square class with
+    a 'size' artribute
+
+    """
+
+    def __init__(self, size):
+        """Initializes a size
+        Args:
+            size - the size of the square
+
+        """
+        self.size = size
