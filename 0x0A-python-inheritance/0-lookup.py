4029acf2c7eb742a99ec04eb98051d870f304f20
musakanneh <mkanneh17@alustudent.com>
2021-01-18 21:52:09 +0200

    Attempted all tasks. Updated betty styles

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Author: Kanneh"""
 
+
 def lookup(obj):
     """A function that returns the list of available
     attributes and methods of an object
-
     Returns a list object
 
     """
