cea0b12dbc9eed502c6aab4cfdc444132f29b14e
musakanneh <mkanneh17@alustudent.com>
2021-01-23 21:11:48 +0200

    Updated rectangle model

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,8 +24,10 @@ class Rectangle(Base):
 
         """
         super().__init__(id)
-        self.width, self.height = height, width
-        self.x, self.y = y, x
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -41,9 +43,9 @@ class Rectangle(Base):
 
         """
         if type(value) not in [int]:
-            raise ValueError("Value must be an integer")
-        if value < 0:
-            return ValueError("x must be >= 0")
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -60,9 +62,9 @@ class Rectangle(Base):
 
         """
         if type(value) not in [int]:
-            raise ValueError("value must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            return ValueError("height must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     @property
@@ -79,9 +81,9 @@ class Rectangle(Base):
 
         """
         if type(value) not in [int]:
-            raise ValueError("value must be an integer")
+            raise TypeError("x must be an integer")
         if value < 0:
-            return ValueError("x must be >= 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -98,7 +100,11 @@ class Rectangle(Base):
 
         """
         if type(value) not in [int]:
-            raise ValueError("value must be an integer")
+            raise TypeError("y must be an integer")
         if value < 0:
-            return ValueError("y must be >= 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """Area of the rectangle"""
+        return self.width * self.height
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle_2.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle_2.py
@@ -12,12 +12,26 @@ class TestRectangle(unittest.TestCase):
         """Tests for Id"""
         r1 = Rectangle(10, 2)
         self.assertEqual(r1.id, 1)
-        print()
+
         r2 = Rectangle(2, 10)
         self.assertEqual(r2.id, 2)
-    
-    def test_valid_input(self):
+
+    def test_multi_id(self):
         """Tests for valid input"""
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+
+    def test_valid_params(self):
+        """Test for valid parameters"""
+        # r4 = Rectangle(3, 5, 2, 8, 108)
+        # self.assertEqual([r4.width, r4.height, r4.x, r4.y, r4.id],
+        #                  [3, 5, 2, 8, 108])
+        # r6 = Rectangle(43, 23)
+        # self.assertEqual([r6.width, r6.height], [43, 23])
+        pass
+
+    def test_default_params(self):
+        """Tests for default paramsters"""
         pass
 
 
