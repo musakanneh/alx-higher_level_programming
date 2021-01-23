878db9f10b1f3183926611f4f1083ca0468a2fcc
musakanneh <mkanneh17@alustudent.com>
2021-01-23 19:16:36 +0200

    Added rectangle class

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """First Rectangle"""
 
+from models.base import Base
+
 
 class Rectangle(Base):
-    """A rectangle class that inherits from the base class
+    """A rectangle class that inherits from
+    the base class
 
     Args:
         Base(model): the inherited model
@@ -11,22 +14,91 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.width
-        self.height
-        """Constructs the rectange's attributes"""
+        """Constructs the rectange's attributes
+
+        Args:
+            height(int): rectangle's height
+            width(int): rectangle's width
+            x(int): input value
+            y(int): input value
+
+        """
+        super().__init__(id)
+        self.width, self.height = height, width
+        self.x, self.y = y, x
 
     @property
     def width(self):
-        self.width = width
+        """Gets the Width of the rectangle"""
+        return self.__width
 
     @width.setter
-    def width(self):
-        self.__width = width
+    def width(self, value):
+        """Sets the Width (private) of the rectangle
+
+        Args:
+            value(int): value of the rectangle
+
+        """
+        if type(value) not in [int]:
+            raise ValueError("Value must be an integer")
+        if value < 0:
+            return ValueError("x must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
-        self.height = height
+        """Gets the height (private) of the rectangle"""
+        return self.__height
 
     @width.setter
-    def height(self):
-        self.__height = height
+    def height(self, value):
+        """Sets the height(private) of the rectangle
+
+          Args:
+            value(int): value of the rectangle
+
+        """
+        if type(value) not in [int]:
+            raise ValueError("value must be an integer")
+        if value < 0:
+            return ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Gets the x value"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Sets x value
+
+         Args:
+            value(int): x value
+
+        """
+        if type(value) not in [int]:
+            raise ValueError("value must be an integer")
+        if value < 0:
+            return ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Gets the x value"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Sets y value
+
+         Args:
+            value(int): y value
+
+        """
+        if type(value) not in [int]:
+            raise ValueError("value must be an integer")
+        if value < 0:
+            return ValueError("y must be >= 0")
+        self.__y = value
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle_2.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle_2.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
 """Test model for the rectangle class"""
 import unittest
-from base.models import Rectangle
+from models.rectangle import Rectangle
+from models.base import Base
 
 
 class TestRectangle(unittest.TestCase):
     """A class test the rectangle model"""
 
-    def test_width(self):
-        """Tests for the rectangle's width"""
+    def test_id(self):
+        """Tests for Id"""
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1, 1)
-        if r1:
-            print("Success!")
-
-    def test_height(self):
-        """Test for the rectangle's height"""
+        self.assertEqual(r1.id, 1)
+        print()
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 2)
+    
+    def test_valid_input(self):
+        """Tests for valid input"""
         pass
 
 
