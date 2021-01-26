d7a4c22a719435c32284371ec1567d5fe1ee9337
musakanneh <mkanneh17@alustudent.com>
2021-01-26 21:23:17 +0200

    Updated test cases

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,17 +7,21 @@ from models.base import Base
 class Rectangle(Base):
     """A rectangle class that inherits from
     the base class
+
     Args:
         Base(model): the inherited model
+
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Constructs the rectange's attributes
+
         Args:
             height(int): rectangle's height
             width(int): rectangle's width
             x(int): input value
             y(int): input value
+
         """
         super().__init__(id)
         self.width = width
@@ -33,10 +37,12 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Sets the Width (private) of the rectangle
+
         Args:
             value(int): value of the rectangle
+
         """
-        if type(value) not in [int]:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be >= 0")
@@ -53,10 +59,11 @@ class Rectangle(Base):
 
           Args:
             value(int): value of the rectangle
+
         """
         if type(value) not in [int]:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
@@ -68,10 +75,12 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Sets x value
+
          Args:
             value(int): x value
+
         """
-        if type(value) not in [int]:
+        if type(value) != int:
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
@@ -85,8 +94,10 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Sets y value
+
          Args:
             value(int): y value
+
         """
         if type(value) not in [int]:
             raise TypeError("y must be an integer")
@@ -101,22 +112,25 @@ class Rectangle(Base):
     def display(self):
         """prints in stdout the Rectangle instance
         with the character #
+
         """
         print("\n" * self.y,  end="")
-        print((" " * self.x + "#" * self.width + '\n') * self.height, end="")
+        print((" " * self.x + "#" * self.__width + '\n') * self.__height, end="")
 
     def __str__(self):
         """String representation of the rectangle class"""
         str_res = "[Rectangle] ({}) {}/{} - {}/{}"\
-            .format(self.id, self.x, self.y, self.width, self.height)
+            .format(self.id, self.x, self.y, self.__width, self.__height)
         return str_res
 
     def update(self, *args, **kwargs):
         """Updates rectangle class and
         assigns an argument to each attribute
+
         Args:
             *args(args):
             **kwargs(kwargs):5-main.py
+
         """
         if not args and not kwargs:
             return
@@ -133,6 +147,7 @@ class Rectangle(Base):
     def to_dictionary(self):
         """Returns the dictionary representation of a
         rectangle
+
         """
         _map = {}
         for key, value in self.__dict__.items():
