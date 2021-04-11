066e54d2db2e3f974a0466bc717b002c371838bc
musakanneh <mkanneh17@alustudent.com>
2021-04-11 21:26:00 +0200

    Updates

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""urllib usage"""
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
-import urllib.parse
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type:', type(html))
-        print('\t- content:', html)
-        print('\t- utf8 content:', html.decode('UTF-8'))
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
--- 0x11-python-network_1/101-starwars.py
+++ 0x11-python-network_1/101-starwars.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
+"""
+Python script that takes in a string and sends
+a search request to the Star Wars API, with pagination
+"""
+from sys import argv
+import requests
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    r = requests.get('https://swapi.co/api/people/?search={}'.format(argv[1]))
+    j = r.json()
+    l = j.get('results')
+    print('Number of results:', j.get('count'))
+    while j.get('next') is not None:
+        r = requests.get(j.get('next'))
+        j = r.json()
+        l += j.get('results')
+    for i in l:
+        print(i.get('name'))
